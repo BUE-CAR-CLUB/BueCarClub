3567c1ca4c5b413579264d3b9887e121416ae785
Berke Kazancı <berkekznc97@gmail.com>
2023-02-28 17:06:06 +0300

    axios kurulumu yapıldı

--- src/CarContext.js
+++ src/CarContext.js
@@ -4,10 +4,15 @@ import { createContext, useState } from "react";
 export const CarContext=createContext();
 
 export const CarProvider=({children})=>{
-    const [car,setCar]=useState("")
+    const [cars,setCars]=useState([])
+
+
+    const fetchCars= async ()=> {
+        
+    }
     
     return (
-        <CarContext.Provider value={{car,setCar}}>
+        <CarContext.Provider value={{cars,setCars}}>
             {children}
         </CarContext.Provider>
     )
