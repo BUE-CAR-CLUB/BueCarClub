d1c7bff9f01382156bdf5f376b3dc8d6c6aaa0d9
unzilemedet <medetunzile@gmail.com>
2023-03-01 10:32:33 +0300

    createCars metodu oluşturuldu.

--- src/CarContext.js
+++ src/CarContext.js
@@ -1,5 +1,7 @@
 import { createContext, useState } from "react";
 import axios from "axios"
+import { async } from "q";
+import { response } from "express";
 
 export const CarContext = createContext();
 
@@ -21,24 +23,32 @@ export const CarProvider = ({ children }) => {
 }
 const editCars= async(id,price,description)=>{
  const response= await axios.put(`http://localhost:3001/cars/${id}`,{
- "price": price,
- "description": description,
+ price: price,
+ description: description,
 })
 const editedCars = cars.map((car)=>{
    if(car.id===id){
   return{...car,...response.data}
   }
   return car;
-
 })
     setCars(editedCars)
-
 }
 
-
-
+const createCars= async(id,newModel,newYear,newPrice,newDescription)=>{
+const response=await axios.post(` http://localhost:3001/cars`,{
+id,
+model:newModel,
+year:newYear,
+price:newPrice,
+description:newDescription,
+})
+    const updatedCars=[...cars, response.data]
+     setCars(createCar)
+}
+   
   return (
-    <CarContext.Provider value={{ cars, setCars, showCars, deleteCars}}>
+    <CarContext.Provider value={{ cars, setCars, showCars, deleteCars, editCars, createCars}}>
       {children}
     </CarContext.Provider>
   );
