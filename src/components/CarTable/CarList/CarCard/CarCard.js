ae89c2488e882427ed2df2b149bb0d5e49ebd8ca
EMRE ŞAHBUDAK <118505939+EmreSahbudak@users.noreply.github.com>
2023-03-01 11:28:45 +0300

    editcars yazıldı

--- src/components/CarTable/CarList/CarCard/CarCard.js
+++ src/components/CarTable/CarList/CarCard/CarCard.js
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CarEdit from './CarEdit'
 
-const CarCard = () => {
+const CarCard = ({car}) => {
+  const [showEdit,setShowEdit]=useState(false)
+
+
+const handleSubmit=()=>{
+  setShowEdit(false)
+}
+
+
+
+let content=<h3>{car.model}</h3>
+if(showEdit){
+  content=<CarEdit car={car} onSubmit={handleSubmit}/>
+}
+
+
   return (
     <div>CarCard</div>
   )
--- src/components/CarTable/CarList/CarList.js
+++ src/components/CarTable/CarList/CarList.js
@@ -5,9 +5,11 @@ import CarCard from "./CarCard/CarCard"
 
 const CarList = () => {
   const {cars}=useContext(CarContext)
+
   const renderCars=cars.map((car)=>{
     return <CarCard key={car.id} car={car}/>
   })
+  
   return (
     <div className='car-list'>
       {renderCars}
