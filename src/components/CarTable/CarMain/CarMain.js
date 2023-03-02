fc0fac65ecc44247bffd15836ed51893d77f5c94
unzilemedet <medetunzile@gmail.com>
2023-03-02 10:16:31 +0300

    CarMain1 html  yazıldı

--- src/components/CarTable/CarMain/CarMain.js
+++ src/components/CarTable/CarMain/CarMain.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const CarMain = () => {
   return (
-    <div>dfsdfsdfd</div>
+    <div className='welcome' id="welcome">
+       <div className='welcomeMain'>
+        <div className='welcomeLeft'>
+          <h1>WELCOME  </h1>
+          <h1>CLASSIC CAR</h1>
+           <h1>SHOWROOM</h1>
+         </div>
+  <div className='welcomeRight'>
+    <img src="" alt="" />
+             </div>
+            </div>
+          </div>
+     
   )
 }
 
