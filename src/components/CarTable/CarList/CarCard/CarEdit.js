a289da946d6cc6d9760dcf6e50451c5bfc4f8871
Berke Kazancı <berkekznc97@gmail.com>
2023-03-09 11:22:01 +0300

    CarEdit refactor edildi

--- src/components/CarTable/CarList/CarCard/CarEdit.js
+++ src/components/CarTable/CarList/CarCard/CarEdit.js
@@ -1,7 +1,8 @@
 import { useContext, useState } from "react";
 import { CarContext } from "../../../../CarContext";
-
+// CarEdit fonksiyonu aracın datalarında değişiklik yapılmasını sağlar
 const CarEdit = ({ car, onSubmit }) => {
+  // Burada car ve onSubmit prop olarak CarEdite aktarıldı.
   const { editCars, showCars } = useContext(CarContext);
   const [model, setModel] = useState(car.model);
   const [year, setYear] = useState(car.year);
@@ -9,20 +10,21 @@ const CarEdit = ({ car, onSubmit }) => {
   const [price, setPrice] = useState(car.price);
   const [description, setDescription] = useState(car.description);
 
+  // Aşağıdaki 2 metot ile price ve description ile ilgili edit işlemleri için kullanıcının girdiği inputu event listener ile yakaldık.
   const handleChangePrice = (e) => {
     setPrice(e.target.value);
   };
   const handleChangeDescription = (e) => {
     setDescription(e.target.value);
   };
-
+  // Bir değişiklik yapıldığında handleSubmit fonksiyonu çağırılır.
   const handleSubmit = (event) => {
     event.preventDefault();
     onSubmit();
-    editCars(car.id, model, year, url, price, description);
+    editCars(car.id, model, year, url, price, description); // Güncel olan veriler burada aktarıldı.
   };
   const handleClick = () => {
-    showCars();
+    showCars(); // save ve close buttonlarına tıklanıldığında işlem bittiği için cardların yeniden gösterilmesi için showCars metodu çalıştırıldı.
   };
 
   return (
