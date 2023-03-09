9c7ba52ac626eed28b909c3470c7009cb55c1abb
Berke Kazancı <berkekznc97@gmail.com>
2023-03-09 11:27:26 +0300

    CarList refactor edildi

--- src/components/CarTable/CarList/CarList.js
+++ src/components/CarTable/CarList/CarList.js
@@ -1,20 +1,19 @@
-import React, { useContext } from 'react'
-import { CarContext } from '../../../CarContext'
-import CarCard from "./CarCard/CarCard"
-
+import React, { useContext } from "react";
+import { CarContext } from "../../../CarContext";
+import CarCard from "./CarCard/CarCard";
 
 const CarList = () => {
-  const {cars}=useContext(CarContext)
-
-  const renderCars=cars.map((car)=>{
-    return <CarCard key={car.id} car={car}/>
-  })
-  
+  const { cars } = useContext(CarContext);
+  const renderCars = cars.map((car) => {
+    // Arabaların verileri map ile dönüldü.Her araba için CarCard componenti oluşturuldu.
+    return <CarCard key={car.id} car={car} />;
+  });
+  // Aşağıda ise CardComponentlerini bir div'e sardık.
   return (
-    <div className='car-list' id='car-list'>
+    <div className="car-list" id="car-list">
       {renderCars}
     </div>
-  )
-}
+  );
+};
 
-export default CarList
+export default CarList;
